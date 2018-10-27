4a7b8c9368740686c15b44989b5ec12f24ecc8d9
Deinonzch <Deinonzch@users.noreply.github.com>
2018-10-27 19:04:33 +0200

    Added models
    - Add model book and author
    - Models are in the admin panel

--- myadminpanel/adminpanel/models.py
+++ myadminpanel/adminpanel/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    second_name = models.CharField(max_length=50)
+    date_of_birth = models.DateField('date of birth')
+
+    def __str__(self):
+        return self.second_name
+
+
+class Book(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    publishing_house = models.CharField(max_length=50)
+    year = models.IntegerField(default=2000)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
