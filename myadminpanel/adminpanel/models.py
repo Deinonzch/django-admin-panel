0a7eba39cb0f5c972ebd5b9d19449448b19cb0d0
Deinonzch <Deinonzch@users.noreply.github.com>
2018-10-29 12:22:45 +0100

    Added model MyPanelAdmin
    - Added model MyPanelAdmin
    - Added model to admin.py

--- myadminpanel/adminpanel/models.py
+++ myadminpanel/adminpanel/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class MyAdminPanel(models.Model):
+    model_name = models.CharField(default='', blank=True, max_length=100)
+
+    def __str__(self):
+        return self.model_name
+
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=50)
