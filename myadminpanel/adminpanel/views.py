4eed3e6d8f96e4f08b9e9198f3a6d573710939c2
Deinonzch <Deinonzch@users.noreply.github.com>
2018-10-29 14:56:28 +0100

    Refactor views

--- myadminpanel/adminpanel/views.py
+++ myadminpanel/adminpanel/views.py
@@ -1,16 +1,11 @@
 from django.shortcuts import render
 from .models import MyAdminPanel
-from django.http import HttpResponse
-from django.template import loader
 
 
 def index(request):
     model_list = MyAdminPanel.objects.order_by('-model_name')
-    template = loader.get_template('adminpanel/index.html')
-    context = {
-        'model_list': model_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'model_list': model_list}
+    return render(request, 'adminpanel/index.html', context)
 
 
 def add_model(request):
